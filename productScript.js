e9d6e4e82b716843cc15f85e0cc2ec157db7036d
abmamun504@gmail.com <abmamun504@gmail.com>
2023-12-02 07:48:22 -0500

    setting data for products

---
+++ productScript.js
@@ -0,0 +1,97 @@
+const menProductsData = [
+    // Men's product data here
+    {
+        image: "./img/men/card-image1.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/men/card-image2.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+      {
+        image: "./img/men/card-image3.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/men/card-image4.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+
+  ];
+
+  const womenProductsData = [
+    // Women's product data here
+    {
+        image: "./img/women/card-image1.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/women/card-image2.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+      {
+        image: "./img/women/card-image3.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/women/card-image4.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+
+  ];
+
+  const featuredItemsData = [
+    // Featured items data here
+    {
+        image: "./img/featured/card-image1.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/featured/card-image2.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+      {
+        image: "./img/featured/card-image3.jpg",
+        name: "Featured Item 1",
+        price: "$999",
+      },
+      {
+        image: "./img/featured/card-image4.jpg",
+        name: "Featured Item 2",
+        price: "$1299",
+      },
+  ];
+
+  const latestProductsData = [
+    // Latest products data here
+    {
+        image: "./img/latest/card-image1.jpg",
+        name: "Latest Product 1",
+        price: "$49",
+      },
+      {
+        image: "./img/latest/card-image2.jpg",
+        name: "Latest Product 2",
+        price: "$79",
+      },
+      {
+        image: "./img/latest/card-image3.jpg",
+        name: "Latest Product 1",
+        price: "$49",
+      },
+      {
+        image: "./img/latest/card-image4.jpg",
+        name: "Latest Product 2",
+        price: "$79",
+      },
+  ];
